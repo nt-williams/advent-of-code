850f90dcba6751a5a09d6d0f2e931cb0ed07bb54
Nick Williams <ntwilliams.personal@gmail.com>
2020-12-02 09:08:24 -0500

    puzzle 2 day 2 r solution.

--- code/day2.R
+++ code/day2.R
@@ -14,3 +14,16 @@ check_psw <- function(rule, target, pw) {
 
 cat("Part 1 answer is:", sum(purrr::pmap_lgl(psw, check_psw)), "\n")
 
+# puzzle 2 ----------------------------------------------------------------
+
+check_psw2 <- function(rule, target, pw) {
+  bnds <- as.numeric(unlist(strsplit(rule, "-")))
+  loc <- unlist(gregexpr(sub(":", "", target), pw))
+  if (sum(bnds %in% loc) == 1) return(TRUE)
+  return(FALSE)
+}
+
+cat("Part 2 answer is:", sum(purrr::pmap_lgl(psw, check_psw2)), "\n")
+
+
+

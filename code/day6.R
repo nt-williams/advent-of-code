72bcdaa24897c855c281e0ce5b773355222383f2
Nick Williams <ntwilliams.personal@gmail.com>
2020-12-06 13:51:24 -0500

    julia solution day 6

--- code/day6.R
+++ code/day6.R
@@ -9,7 +9,7 @@ read_answers <- function(file) {
 ans <- read_answers("data/day6")
 
 # puzzle 1
-sum(map_dbl(ans, ~ length(unique(unlist(strsplit(paste0(.x, collapse = ""), ""))))))
+sum(map_dbl(ans, ~ length(unique(unlist(strsplit(.x, ""))))))
 
 # puzzle 2
 sum(map_dbl(ans, ~ sum(table(unlist(strsplit(.x, ""))) == length(.x))))
---
+++ code/day6.jl
@@ -0,0 +1,26 @@
+function read_answers(file)
+	lines = readlines(file)
+	answers = [[]]
+	for line in lines
+		if line == ""
+			push!(answers, [])
+		else
+			push!(last(answers), line)
+		end
+	end
+	answers
+end
+
+answers = read_answers("data/day6")
+
+# puzzle 1
+[length(unique(split(join(i), ""))) for i in answers] |> sum
+
+# puzzle 2
+c = 0
+for a in answers
+	letters = split(join(a), "")
+	for i in unique(letters), l in length(a)
+		global c += count(x -> x == i, letters) == l
+	end
+end
